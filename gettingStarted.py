49bcd95898552c723920561c67b30d92ce204d23
Moeis Azam Butt <mab10269@nyu.edu>
2026-09-06 13:52:17 -0400

    Refactor welcome_assignment_answers function

--- gettingStarted.py
+++ gettingStarted.py
@@ -22,24 +22,26 @@ def welcome_assignment_answers(question):
     return(answer)
 
 if __name__ == "__main__":
-    debug_question = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decrypt a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is it possible to decode a message without a key? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "Is a hashed message supposed to be un-hashed? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "is MD5 a secured hashing algorithm? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"
-    print(welcome_assignment_answers(debug_question))
-    debug_question = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
-    print(welcome_assignment_answers(debug_question))
+    debug_question1 = "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
+    debug_question2 = "Are encoding and encryption the same? - Yes/No"
+    debug_question3 = "Is it possible to decrypt a message without a key? - Yes/No"
+    debug_question4 = "Is it possible to decode a message without a key? - Yes/No"
+    debug_question5 = "Is a hashed message supposed to be un-hashed? - Yes/No"
+    debug_question6 = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
+    debug_question7 = "is MD5 a secured hashing algorithm? - Yes/No"
+    debug_question8 = "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number"
+    debug_question9 = "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number"
+
+    print(welcome_assignment_answers(debug_question1))
+    print(welcome_assignment_answers(debug_question2))
+    print(welcome_assignment_answers(debug_question3))
+    print(welcome_assignment_answers(debug_question4))
+    print(welcome_assignment_answers(debug_question5))
+    print(welcome_assignment_answers(debug_question6))
+    print(welcome_assignment_answers(debug_question7))
+    print(welcome_assignment_answers(debug_question8))
+    print(welcome_assignment_answers(debug_question9))
+
 
 #Questions & Answers:
 #"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": pcap
