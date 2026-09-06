a525d0430259665d7209357e5b764ecde9927f6f
Moeis Azam Butt <mab10269@nyu.edu>
2026-09-06 14:30:49 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -41,15 +41,3 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question7))
     print(welcome_assignment_answers(debug_question8))
     print(welcome_assignment_answers(debug_question9))
-
-
-#Questions & Answers:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?": pcap
-#"Are encoding and encryption the same? - Yes/No": No
-#"Is it possible to decrypt a message without a key? - Yes/No": No
-#"Is it possible to decode a message without a key? - Yes/No": Yes
-#"Is a hashed message supposed to be un-hashed? - Yes/No": No
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":64eec04f7bdbd459dc455d1151105b33957ff61c7118b7f23606e31b92b52b8d
-#"Is MD5 a secured hashing algorithm? - Yes/No": No
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 5
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 3
