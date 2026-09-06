e535db3e13efb3f1e2e15ab561d3b170dee9d1a1
Moeis Azam Butt <mab10269@nyu.edu>
2026-09-06 18:18:54 -0400

    Refactor welcome_assignment_answers function

--- gettingStarted.py
+++ gettingStarted.py
@@ -16,7 +16,7 @@ def welcome_assignment_answers(question):
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 5
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
-        answer = "3"
+        answer = 3
     else:
         answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
     return answer
